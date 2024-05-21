b0aeb7557879d49f088499776de2b7324447f966
M4rckR <marcromerogar4@gmail.com>
2024-05-21 00:10:50 -0500

    Repositorio de clima con react iniciado

---
+++ src/AppClima.jsx
@@ -0,0 +1,20 @@
+
+
+export const AppClima = () => {
+  return (
+    <header className="container mx-auto py-4">
+        <section className="flex justify-between items-center">
+            <h1 className="text-2xl font-bold">Clima con Marcos</h1>
+        <div className="flex gap-4">
+            <a className="bg-green-700 text-white py-2 px-6 rounded-lg font" 
+                href="#">Ingresar
+            </a>
+            <a className="bg-green-700 text-white py-2 px-6 rounded-lg font" 
+                href="#">Iniciar sesion
+            </a>
+        </div>
+        </section>
+    </header>
+  )
+}
+
